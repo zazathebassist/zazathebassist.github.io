5061e91ca57dee8418a77271a76923faae7c31fe
Emmanuel Medina <emedina94@gmail.com>
2020-09-10 21:46:08 -0700

    Uploading Photography Post
    - Upload Photography Post
    - Upload new images for Photography post
    - Update About and Link pages

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,44 +3,59 @@ var store = [{
         "excerpt":"I am leaving this default page up, both because I really like Jekyll and want to emphasize that this blog was done in Jekyll and anyone can do it... and it serves as good documentation for me. You’ll find this post in your _posts directory. Go ahead and edit it...","categories": ["jekyll","update"],
         "tags": [],
         "url": "http://localhost:4000/blog/welcome-to-jekyll/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "Test Blog Part 3!",
         "excerpt":"I've had two blogs, and abandoned both. So... I'm launching my third blog... What am I Doing Here. I ask myself that all too much. This is the 3rd blog I try(4th if you count tumblr… I don’t) in earnest to start. And it’s been up in some capacity since...","categories": ["project","update"],
         "tags": [],
         "url": "http://localhost:4000/blog/test-blog-part-3/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "Back to Living in Fear",
         "excerpt":"When I was a kid, I had one big fear. Most kids were scared of heights, or the dark, or the monster under the bed. I was scared of being pulled over. I was scared that the cops were going to take away my mom and dad. I’m a child...","categories": ["personal"],
         "tags": [],
         "url": "http://localhost:4000/blog/back-in-fear/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "Coming Back to Magic",
         "excerpt":"I left Magic about two years ago. It was a slow fade, and once I was done I thought I was out. I thought I quit this Cardboard Crack OH NO You don’t just leave Magic. And I’m so glad I’m back. Backstory. I stopped playing Magic about two years...","categories": ["personal","games","magic"],
         "tags": [],
         "url": "http://localhost:4000/blog/coming-back-to-magic/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "Accidental Disconnect from the Internet",
         "excerpt":"It’s been an odd month or two for me. For the first time in as long as I can remember, I’ve been mostly disconnected from the internet. This wasn’t really any kind of planned event. I had recently become more active on the Do By Friday discord, was fairly active...","categories": ["personal"],
         "tags": [],
         "url": "http://localhost:4000/blog/accidental-disconnect/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "2019: Year of Content",
         "excerpt":"I’ve been thinking about my 2019 plan for a while. And after the absolute failure that 2018: Year of Less was, but also the kinda semi success of 2017: Year of Completion, and the success of my 2016: Year of Me, I was excited to talk about my new theme....","categories": ["personal"],
         "tags": [],
         "url": "http://localhost:4000/blog/year-of-content/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "iOS Game Reviews: Elder Scrolls Blades",
         "excerpt":"I want to love Elder Scrolls: Blades. I do. I really really do. I’m starting this off by saying that I am a huge fan of the Elder Scrolls series. I have put more hours than I can count into Oblivion and almost as many into Skyrim. Elder Scrolls Online...","categories": ["gaming","reviews"],
         "tags": [],
         "url": "http://localhost:4000/blog/ios-game-review-elder-scrolls-blades/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "Oko: A Returning Player's Perspective",
         "excerpt":"Now, I started writing this before Oko was banned but even after the Ban, I feel like I have something to say. So I was a very into Magic a few years ago. I came into the game at the end of the Return to Ravnica block, my first event...","categories": ["personal","games","magic"],
         "tags": [],
         "url": "http://localhost:4000/blog/Oko-A-Returning-Players-Perspective/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "Top Ten Games of the Decade (2010-2019)",
         "excerpt":"It’s the end of the 2010s and I really want to reflect on my life. But I feel like I’m in a flux part of my life and with so much happening around me, I thought it’d be easier to just do games of the decade. A bit about me,...","categories": ["personal","games"],
         "tags": [],
         "url": "http://localhost:4000/blog/top-ten-games-decade/",
-        "teaser":null}]
+        "teaser": null
+      },{
+        "title": "iPhone Photography, and Finding Photography Again",
+        "excerpt":"I was very into photography a few years ago. Decent DSLR, lenses, accessories, the whole deal. But for various reasons my love of photography left me. The camera was too heavy to bring, I wasn’t good enough, and I had some personal reasons. So I kinda stopped photography. The DSLR...","categories": ["personal","games"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/iphone-photography/",
+        "teaser": null
+      }]
